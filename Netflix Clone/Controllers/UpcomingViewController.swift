249c01049987c45daec2d0e31d4e0d54a67f50ed
KingLegend12 <firedeadman30@gmail.com>
2023-05-21 00:54:17 +0100

    Initial Commit

---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Driss Jaidi on 7/5/2023.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
---
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Driss Jaidi on 21/5/2023.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemGray
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
